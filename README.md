c06cce25052e303a8b97e45bd6989a38f30f5900
oxarbitrage <oxarbitrage@gmail.com>
2017-12-02 13:43:18 -0800

    Use elasticsearch-dsl-py

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Bitshares ElasticSearch Wrapper - BESW
+
+## Install
+
+- Install python elasticsearch low level lib:
+
+`pip install elasticsearch`
+
+Install python elasticsearch high level lib to easy query:
+
+`pip install elasticsearch-dsl`
+
+```
+git clone https://github.com/oxarbitrage/bitshares-es-wrapper.git
+export FLASK_APP=wrapper.py
+flask run --host=0.0.0.0
+```
+ 
+ ## Available Calls
+ 
+ ### get_account history
+ 
+Get all operations in history with pager, similar to bitshares node call.
+ 
+ #### Samples:
+ 
+ get history of account sort by date:
+
+`http://209.188.21.157:5000/get_account_history?account_id=1.2.282&from=100&size=10&sort_by=block_data.block_time`  
+
+sort by operation_time
+
+`http://209.188.21.157:5000/get_account_history?account_id=1.2.282&from=100&size=10&sort_by=operation_type`
+
+reverse order:
+
+`http://209.188.21.157:5000/get_account_history?account_id=1.2.282&from=100&size=10&sort_by=-operation_type`
--- wrapper.py
+++ wrapper.py
@@ -4,73 +4,31 @@ from flask import request
 
 from datetime import datetime
 from elasticsearch import Elasticsearch
-
+from elasticsearch_dsl import Search
 
 es = Elasticsearch(timeout=30)
 app = Flask(__name__)
 
-@app.route('/get_data')
-def get_last_transactions():
-
-    type = request.args.get('type')
+@app.route('/get_account_history')
+def get_account_history():
+    account_id = request.args.get('account_id')
+    from_ = request.args.get('from')
     size = request.args.get('size')
-    field = request.args.get('field')
-    sort = request.args.get('sort')
-    order = request.args.get('order')
-    gte = request.args.get('gte')
-    lte = request.args.get('lte')
-    term = request.args.get('term')
+    sort_by = request.args.get('sort_by')
 
-    sort =  {"sort" : [ { sort : {"order" : order}}]}
+    s = Search(using=es, index="graphene-*", extra={"size": size}) \
+        .query("match", account_history__account=account_id) \
+        .extra(from_=from_) \
+        .sort(sort_by)
 
-    # samples
-    ##@# type = aggregator
-    # size = 10
-    # sort =  block_data.block_time
-    # field = block_data.trx_id.keyword
-    # order = "desc"
-    # gte = "now-1h"
-    # lte = "now"
-
-    # for data field can be account_history.account.keyword and term 1.2.282
-
-    body = sort.copy()
-
-    if type == "aggregator":
-        aggs = {"aggs": { "group_by_data": { "terms": { "field": field, "size": size } } } }
-        query = {"query": {"bool": {"must": [{
-            "range": {"block_data.block_time": {"gte": gte, "lte": lte}}}]}}}
-
-        body.update(aggs)
-        body.update(query)
-
-    elif type == "data":
-        query = {"query": {"bool": {"must": [{
-            "range": {"block_data.block_time": {"gte": gte, "lte": lte}}}, {"term": {field: term}}]}}}
-
-        body.update(query)
-
-    fromm = {"from" : 0}
-    size = {"size": size}
-
-    body.update(fromm)
-    body.update(size)
-
-    res = es.search(index="graphene-*", body=body)
-
+    response = s.execute()
+    #print response
     results = []
-
-    if type == "aggregator":
-        for b in res['aggregations']['group_by_data']["buckets"]:
-            results.append({"key": b["key"], "doc_count": b["doc_count"]})
-
-    elif type == "data":
-        for d in res["hits"]["hits"]:
-            results.append(d["_source"])
-
+    print s.count()
+    for hit in response:
+        results.append(hit.to_dict())
+        #print(hit.meta.score, hit.account_history.account)
     return jsonify(results)
-
-
 
 if __name__ == '__main__':
     app.run()

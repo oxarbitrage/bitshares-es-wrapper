391d5ebd3464f9f8f057821c888056df260c5de3
oxarbitrage <oxarbitrage@gmail.com>
2017-12-03 07:26:30 -0800

    Making get_Account_history call full featured

--- wrapper.py
+++ wrapper.py
@@ -4,27 +4,44 @@ from flask import request
 
 from datetime import datetime
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
+from elasticsearch_dsl import Search, query, Q, DocType, utils
+from elasticsearch_dsl.exceptions import IllegalOperation
 
 es = Elasticsearch(timeout=30)
 app = Flask(__name__)
 
 @app.route('/get_account_history')
 def get_account_history():
-    account_id = request.args.get('account_id')
-    from_ = request.args.get('from')
-    size = request.args.get('size')
-    sort_by = request.args.get('sort_by')
 
-    s = Search(using=es, index="graphene-*", extra={"size": size}) \
-        .query("match", account_history__account=account_id) \
-        .extra(from_=from_) \
-        .sort(sort_by)
+    account_id = request.args.get('account_id') if 'account_id' in request.args else False
+    operation_type = request.args.get('operation_type') if 'operation_type' in request.args else False
+    from_ = request.args.get('from_') if 'from_' in request.args else 0
+    size = request.args.get('size') if 'size' in request.args else 10
+    from_date = request.args.get('from_date') if 'from_date' in request.args else "2015-10-10"
+    to_date = request.args.get('to_date') if 'to_date' in request.args else "now"
+    sort_by = request.args.get('sort_by') if 'sort_by' in request.args else "block_data__block_time"
+
+    s = Search(using=es, index="graphene-*", extra={"size": size, "from": from_})
+
+    if account_id and operation_type:
+        q = Q("match", account_history__account=account_id) & Q("match", operation_type=operation_type)
+    elif account_id and not operation_type:
+        q = Q("match", account_history__account=account_id)
+    elif not account_id and operation_type:
+        q = Q("match", operation_type=operation_type)
+
+    #s.query = q
+    range_query = Q("range", block_data__block_time={'gte': from_date, 'lte': to_date})
+    s.query = q & range_query
+
+    s = s.sort(sort_by)
+
+    print s.to_dict()
 
     response = s.execute()
     #print response
     results = []
-    print s.count()
+    #print s.count()
     for hit in response:
         results.append(hit.to_dict())
         #print(hit.meta.score, hit.account_history.account)
